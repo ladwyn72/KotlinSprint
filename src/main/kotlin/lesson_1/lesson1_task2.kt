3c13b35b935befffd9a479948e2abadc23f024ee
ladyn <tgabidullin72@gmail.com>
2024-01-19 03:35:30 +0300

    KS-1-2-employeesCount

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+fun main() {
+    val orderCount : Int = 75
+    val thankYou : String = "спасибо за покупку"
+    println(orderCount)
+    println(thankYou)
+    var employeesCount = 2000
+    // println(employeesCount)
+    employeesCount--
+    println(employeesCount)
+
+
+}
