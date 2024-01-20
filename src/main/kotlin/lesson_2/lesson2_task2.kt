e849a17d7fdb93f5407cba20065a57bdd5682d61
ladyn <tgabidullin72@gmail.com>
2024-01-20 11:54:20 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,9 @@
+package lesson_2
+fun main (){
+    val permanentEmployeesSalaryExpenses = 50 * 30000
+    val totalSalaryExpensesAfterInterns = permanentEmployeesSalaryExpenses + (30 * 20000)
+    val averageSalaryAfterInterns = totalSalaryExpensesAfterInterns / 80
+    println(permanentEmployeesSalaryExpenses)
+    println(totalSalaryExpensesAfterInterns)
+    println(averageSalaryAfterInterns)
+}
