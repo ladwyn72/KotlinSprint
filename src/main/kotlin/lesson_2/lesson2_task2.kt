05a1d5e345a6f34ed3b3bf9427437e350f6d0a5d
ladyn <tgabidullin72@gmail.com>
2024-01-22 10:37:06 +0300

    KS-2-2 refactor

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,8 +1,15 @@
 package lesson_2
-fun main (){
-    val permanentEmployeesSalaryExpenses = 50 * 30000
-    val totalSalaryExpensesAfterInterns = permanentEmployeesSalaryExpenses + (30 * 20000)
-    val averageSalaryAfterInterns = totalSalaryExpensesAfterInterns / 80
+
+fun main() {
+    val permanentEmployees = 50
+    val permanentEmployeesSalary = 30000
+    val internCount = 30
+    val internSalary = 20000
+
+    val permanentEmployeesSalaryExpenses = permanentEmployees * permanentEmployeesSalary
+    val totalSalaryExpensesAfterInterns = permanentEmployeesSalaryExpenses + (internCount * internSalary)
+    val averageSalaryAfterInterns = totalSalaryExpensesAfterInterns / (permanentEmployees + internCount)
+
     println(permanentEmployeesSalaryExpenses)
     println(totalSalaryExpensesAfterInterns)
     println(averageSalaryAfterInterns)
