609ef0bb3dfdd0a1320edafe29d2208037f7ba70
ladyn <tgabidullin72@gmail.com>
2024-01-22 09:41:18 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+import java.time.LocalTime
+
+fun main (){
+    val departureTime = LocalTime.of(9,39,0)
+    val travelTime : Long = 457
+    val arrivalTime = departureTime.plusMinutes(travelTime)
+    println(arrivalTime)
+
+
+}
