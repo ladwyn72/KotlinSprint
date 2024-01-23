2e5b3831b7fd3fbe8301a0e9704ac6796f2bcf98
ladyn <tgabidullin72@gmail.com>
2024-01-23 21:36:17 +0300

    KS-2-3 refactor

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -2,9 +2,9 @@ package lesson_2
 
 import java.time.LocalTime
 
-fun main (){
-    val departureTime = LocalTime.of(9,39,0)
-    val travelTime : Long = 457
+fun main() {
+    val departureTime = LocalTime.of(9, 39, 0)
+    val travelTime: Long = 457
     val arrivalTime = departureTime.plusMinutes(travelTime)
     println(arrivalTime)
 
