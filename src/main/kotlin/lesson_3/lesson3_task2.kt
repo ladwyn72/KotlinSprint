9206395b3b8139fef1e9f102fcdd70cba482d283
ladyn <tgabidullin72@gmail.com>
2024-01-24 03:32:58 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val fathername = "Сергеевна"
+
+    val surnameBefore = "Андреева"
+    val surnameAfter = "Сидорова"
+
+    val ageBefore = 20
+    val ageAfter = 22
+
+    println("$surnameBefore $name $fathername $ageBefore")
+    println("$surnameAfter $name $fathername $ageAfter")
+}
