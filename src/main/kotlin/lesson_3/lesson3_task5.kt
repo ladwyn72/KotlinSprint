ef9a43a15332c350a978d48b2e35a918ad52a5ce
ladyn <tgabidullin72@gmail.com>
2024-01-25 18:46:59 +0300

    KS-3-5

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+    val string = "D2-D4;0"
+    val separatorCharacter1 = "-"
+    val separatorCharacter2 = ";"
+    val list = string.split(separatorCharacter1,separatorCharacter2)
+
+
+    val startLocation: String = list[0]
+    val endLocation : String = list[1]
+    val moveNumber : String = list[2]
+    println("откуда $startLocation куда $endLocation номер хода $moveNumber")
+
+
+}
